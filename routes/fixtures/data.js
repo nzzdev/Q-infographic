f2d15e1ffe581468cf30a675d93ecd53c9ba9d7a
benib <beni.buess@nzz.ch>
2019-03-05 16:18:50 +0100

    (tests) test all rendering-info routes

--- routes/fixtures/data.js
+++ routes/fixtures/data.js
@@ -1,19 +1,20 @@
-const fixtureDataDirectory = '../../resources/fixtures/data'
+const fixtureDataDirectory = "../../resources/fixtures/data";
 
 // provide every fixture data file present in ../../resources/fixtures/data
 // has to be in sync with files created in build task - see ../../tasks/build.js
 const fixtureData = [
-  require(`${fixtureDataDirectory}/basic.json`)
-]
+  require(`${fixtureDataDirectory}/2-variants-no-min-width.json`),
+  require(`${fixtureDataDirectory}/2-variants.json`)
+];
 
 module.exports = {
-  path: '/fixtures/data',
-  method: 'GET',
+  path: "/fixtures/data",
+  method: "GET",
   options: {
-    tags: ['api'],
+    tags: ["api"],
     cors: true
   },
   handler: (request, h) => {
-    return fixtureData
+    return fixtureData;
   }
-}
+};
